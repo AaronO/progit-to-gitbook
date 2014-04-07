aa50594a3b1bb23825e2db02cc202a73a8b26b4d
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-04-07 00:02:45 -0700

    Support all progit languages
    Move langs to separate module and generate LANGS.md file

--- bin/progit-to-gitbook.js
+++ bin/progit-to-gitbook.js
@@ -4,45 +4,27 @@ var Q = require('q');
 var _ = require('lodash');
 var path = require('path');
 
+var fs = require('../lib/fs');
 
+var langs = require('../lib/langs');
 var convert = require('../');
 
-var LANGS = [
-    'ar',
-    'az',
-    'be',
-    'ca',
-    'cs',
-    'de',
-    'en',
-    'eo',
-    'es-ni',
-    'es',
-    'fi',
-    'fr',
-    'hi',
-    'hu',
-    'id',
-    'it',
-    'ja',
-    'ko',
-    'mk',
-    'nl',
-    'no-nb',
-    'pl',
-    'pt-br',
-    'ro',
-    'ru',
-    'sr',
-    'th',
-    'tr',
-    'vi',
-    'zh-tw',
-    'zh',
-];
+var LANGS = _.keys(langs);
 
 var DIR = path.resolve(process.argv[2]);
 var OUT = path.resolve(process.argv[3]);
+
+
+function listItem(title, path) {
+    return '* ['+title+']'+'('+path+')';
+}
+
+function genLangs() {
+    return _.map(langs, function(name, code) {
+        return listItem(name, code);
+    }).join('\t\n');
+}
+
 
 if(!DIR || !OUT) {
     console.error('Must provide input & output directory');
@@ -54,4 +36,7 @@ Q.all(_.map(LANGS, function(lang) {
         path.join(DIR, lang),
         path.join(OUT, lang)
     );
-}));
+}))
+.then(function() {
+    return fs.writeFile(path.join(OUT, 'LANGS.md'), genLangs());
+});
